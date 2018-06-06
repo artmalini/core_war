da7b08c5111437c60e99b531548a5839e3e1220a
tvertohr <timon8106@gmail.com>
2018-06-06 12:56:22 +0300

    reading

--- virtual_machine/vm_main.c
+++ virtual_machine/vm_main.c
@@ -10,10 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "vm.h"
 
-int main(void)
+// int main() {
+
+//  initscr();
+//  noecho();
+//  curs_set(FALSE);
+
+//  mvprintw(0, 0, "Hello, world!");
+//  refresh();
+
+//  sleep(1);
+
+//  endwin();
+// }
+
+int main(int argc, char **argv)
 {
-	printf("Hello\n");
-	return 0;
+	int fd;
+	int ret;
+	int i = 0;
+	char buf[BUF_SIZE + 1];
+	t_player p;
+
+	if (argc != 2)
+	{
+		ft_printf("error\n");
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	while ((ret = read(fd, buf, BUF_SIZE + 1)) > 0)
+	{
+		buf[ret] = '\0';
+		ft_printf("%s\n", buf);
+		if (i <= 1)
+			p.magic = ft_strjoin(p.magic, ft_strsub(buf, 0, BUF_SIZE));
+		else if (i <= PROG_NAME_LENGTH / 2 + 2)
+			p.bot_name = ft_strjoin(p.bot_name, ft_strsub(buf, 0, BUF_SIZE));
+		else if (i == PROG_NAME_LENGTH / 2 + 4)
+			p.size_exec = ft_atoi(buf); //здесь должно быть переведение з 16 в 10 систему, но пока просто атои
+		else if (i <= PROG_NAME_LENGTH / 2 + 4 + COMMENT_LENGTH / 2)
+			p.comment = ft_strjoin(p.comment, ft_strsub(buf, 0, BUF_SIZE));
+		else if (i >  PROG_NAME_LENGTH / 2 + 4 + COMMENT_LENGTH / 2 + 1)
+			p.ex_code = ft_strjoin(p.ex_code, ft_strsub(buf, 0, BUF_SIZE));
+		i++;
+	}
+	return (0);
 }
