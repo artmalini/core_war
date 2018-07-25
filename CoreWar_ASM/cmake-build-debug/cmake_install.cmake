59ec3f123633c7e6ea6eea29c8d4a321289de002
vmakahon <vmakahon@gmail.com>
2018-07-26 00:58:34 +0300

    [CoreWar]: Day 49 -  vmakahon (auto-push)

--- CoreWar_ASM/cmake-build-debug/cmake_install.cmake
+++ CoreWar_ASM/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/vmakahon/UNIT_Factory/JetBrains/CLion/CoreWar_ASM
+# Install script for directory: /Users/vmakahon/CoreWar_Full/CoreWar_ASM
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/vmakahon/UNIT_Factory/JetBrains/CLion/CoreWar_ASM/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/vmakahon/CoreWar_Full/CoreWar_ASM/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
